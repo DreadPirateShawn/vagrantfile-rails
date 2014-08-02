b90d3ce71e2720545b1241cc505ab6f97251fd73
DreadPirateShawn <dreadpirateshawn@gmail.com>
2014-08-01 21:22:07 -0700

    Packages: Moved database-related packages into separate line.

--- install-apt.sh
+++ install-apt.sh
@@ -4,10 +4,13 @@ echo "#########"
 echo "# apt-get"
 
 apt-get update
-apt-get install -y build-essential openssl libreadline6 zlib1g zlib1g-dev git-core sqlite3 libsqlite3-dev postgresql libpq-dev curl python-software-properties nodejs libcurl4-openssl-dev libreadline6-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion apt-transport-https ca-certificates libgdbm-dev libffi-dev
+apt-get install -y build-essential openssl libreadline6 zlib1g zlib1g-dev git-core curl python-software-properties nodejs libcurl4-openssl-dev libreadline6-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion apt-transport-https ca-certificates libgdbm-dev libffi-dev
 
 # lynx (alternative to curl for some use cases)
 apt-get install -y lynx
+
+# database support
+apt-get install -y sqlite3 libsqlite3-dev postgresql libpq-dev
 
 # rspec (rails in general?) requires these.
 apt-get install -y libgtk2.0 libgtkmm-2.4 libgtkmm-3.0
