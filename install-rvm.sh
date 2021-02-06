7365b003cb97452a3bee53aae0866ad00bc9e990
Shawn FH <dreadpirateshawn@gmail.com>
2021-02-06 12:55:13 -0800

    Uplift to Ubuntu 20.04 and trim scope.

--- install-rvm.sh
+++ install-rvm.sh
@@ -1,17 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
+
+export DEBIAN_FRONTEND=noninteractive
 
 
 echo "############"
-echo "# RVM check: $1"
+echo "# RVM check"
 
 if which rvm
 then
   echo "# ~> RVM already installed"
 else
   # Instructions per https://rvm.io/rvm/install
-  command curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
-  command curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg --import -
-  curl -sSL https://get.rvm.io | bash -s $1
+  sudo apt-get install software-properties-common
+  sudo apt-add-repository -y ppa:rael-gc/rvm
+  sudo apt-get -qq update
+  sudo apt-get install -y rvm
 fi
 
 
@@ -19,4 +22,4 @@ echo "###########################"
 echo "# Modify vagrant user group"
 
 sudo usermod --append --groups rvm vagrant
-
+groups vagrant
